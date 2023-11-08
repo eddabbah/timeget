18f8188f99667be6a50ad2bda9757ba4e34aa253
eddabbah mohamed <eddabbah@gmail.com>
2023-11-08 20:23:08 +0100

    Update modify_log.py

--- modify_log.py
+++ modify_log.py
@@ -5,4 +5,4 @@ log_file_path = "file.log"
 with open(log_file_path, "a") as log_file:
     current_time = datetime.datetime.now()
     log_file.write(f"{current_time}: Log entry added by GitHub Actions\n")
-    print(log_file.write)
+    print(log_file.write(f"{current_time}: Log entry added by GitHub Actions\n"))
